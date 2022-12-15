5aa5f6aeb72f52962ed49ea8b6a7f930694ffda1
Shivasharma17 <shivathink17@gmail.com>
2022-12-15 23:15:44 +0530

    adding screenshots

--- home/urls.py
+++ home/urls.py
@@ -6,10 +6,10 @@ urlpatterns = [
     
     
     #landingPage
-    path(' ',views.landingpage,name="landingpage"),
+    path('',views.landing,name="landing"),
 
     # Home
-    path('', views.home,name="home"),
+    path('home/', views.home,name="home"),
 
     # Auth
     path('register', views.register,name="register"),
@@ -21,6 +21,10 @@ urlpatterns = [
     path('create-group',views.creategroup,name="create-group"),
     path('group/<str:id>',views.groupdetails,name="groupdetails"),
     path('group/<str:id>/add-member',views.addmember,name="addmember"),
+    #delete-group
+    path("group/<str:id>/deletegroup", views.deletegroup, name="delete Group"),
+
+    # path("group/<str:id>/deleteexpense",views.deleteexpense,name="deleteexpense"),
 
     # calculate
     path('sendmsg',views.sendmsg,name="sendmsg"),
@@ -28,6 +32,15 @@ urlpatterns = [
     # history
     path('history',views.history,name="history"),
 
-    #history
+    #wallet
     path('wallet',views.wallet,name="wallet"),
+    
+    #support
+    path('support',views.support,name="support"),
+
+    #profile
+    path('profile',views.profile,name="profile")
+
+    
+
 ]
--- split/settings.py
+++ split/settings.py
@@ -131,7 +131,12 @@ STATICFILES_DIRS = (
         os.path.join(BASE_DIR, 'static'),
     )
 
-
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'sk5140712@gmail.com'
+EMAIL_HOST_PASSWORD = 'wndyhumxakkewdkl'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
